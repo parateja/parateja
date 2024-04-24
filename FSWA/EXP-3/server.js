3985b13f70233307540547894349cf0b2b83ae2e
parateja <160629703+parateja@users.noreply.github.com>
2024-04-24 15:47:23 +0530

    Add files via upload

--- FSWA/EXP-3/server.js
+++ FSWA/EXP-3/server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
 app.use(express.static('static'));
-app.listen(3000,function(){
+app.listen(6000,function(){
 console.log('Example app listening on port 3000!');
 });
---
+++ FSWA/EXP-4/app.js
@@ -0,0 +1,6 @@
+var contentNode=document.getElementById('contents')
+var component=React.CreateElement(
+    'h1',
+    null,
+    'hello teja!'  
+);
